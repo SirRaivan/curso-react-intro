9f89705eff9cbf57a444e5213519eb14ec6bca4a
Ivan Arturo Cio Toral <ivanarturo.cio@gmail.com>
2023-09-27 17:12:50 -0600

    add completed css styles

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,7 @@ import './TodoCounter.css';
 function TodoCounter({ total, completed}) {
   return (
     <h1>
-      Has completados {completed} de {total} TODOS
+      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
     </h1>
   )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,14 @@
+import './TodoSearch.css';
+
 function TodoSearch() {
   return (
-    <input placeholder="Cortar cebolla"/>
+    <input 
+      placeholder="Buscar..."
+      className='TodoSearch'
+      onChange={(event) => {
+        
+      }}
+      />
   )
 }
 
