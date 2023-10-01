44e3568964f73b54e1e54d935eba97978d41a8cb
Ivan Arturo Cio Toral <ivanarturo.cio@gmail.com>
2023-10-01 15:12:20 -0600

    adding states and function search and filter todos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
@@ -8,19 +9,38 @@ import './App.css';
 const defaultTodos = [
   {text: 'Cortal cebolla', completed: true},
   {text: 'Cortal cebollin', completed: false},
-  {text: 'Cortal cebo', completed: true},
+  {text: 'Cortal cebo', completed: false},
   {text: 'Cortal olla', completed: false},
 ]
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  )
+
+  console.log(searchValue)
+
   return (
     <>
 
-      <TodoCounter total={20} completed={15}/>
-      <TodoSearch />
+      <TodoCounter total={totalTodos} completed={completedTodos}/>
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
             key={todo.text} 
             text={todo.text}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({searchValue, setSearchValue}) {
   return (
     <input 
       placeholder="Buscar..."
       className='TodoSearch'
+      value={searchValue}
       onChange={(event) => {
-        
+        setSearchValue(event.target.value)
       }}
       />
   )
