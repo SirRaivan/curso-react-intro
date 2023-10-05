acb674a836490ebc856cd88fa575480ef1696738
Ivan Arturo Cio Toral <ivanarturo.cio@gmail.com>
2023-10-05 12:47:16 -0600

    add icons, completed and deleted functions

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,6 +1,13 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed}) {
+  if (total == completed) {
+    return (
+      <h1>
+        Felicidades has completado todo los TODOs
+      </h1>
+    )
+  } else
   return (
     <h1>
       Has completado <span>{completed}</span> de <span>{total}</span> TODOs
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,30 @@
+import { CompleteIcon } from './CompleteIcon';
+import { DeleteIcon } from './DeleteIcon';
 import './TodoItem.css';
 
 function TodoItem(props) {
   return (
     <li>
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>V</span>
+      <CompleteIcon 
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
+      {/* <span 
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={props.onComplete}
+      >
+        V
+      </span> */}
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-      <span className='Icon Icon-delete'>X</span>
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
+      {/* <span 
+        className='Icon Icon-delete'
+        onClick={props.onDelete}
+      >
+        X
+      </span> */}
     </li>
   );
 }
