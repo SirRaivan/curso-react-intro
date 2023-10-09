a29fbe19869db69ba480c85f27b7fdb8d38e1b95
Ivan Arturo Cio Toral <ivanarturo.cio@gmail.com>
2023-10-09 09:07:36 -0600

    add localstorage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,28 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
 
-const defaultTodos = [
-  {text: 'Cortal cebolla', completed: true},
-  {text: 'Cortal cebollin', completed: false},
-  {text: 'Cortal cebo', completed: false},
-  {text: 'Cortal olla', completed: false},
-]
+// const defaultTodos = [
+//   {text: 'Cortal cebolla', completed: true},
+//   {text: 'Cortal cebollin', completed: false},
+//   {text: 'Cortal cebo', completed: false},
+//   {text: 'Cortal olla', completed: false},
+// ]
+// localStorage.setItem('TODOS_V1',JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1')
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -28,13 +41,19 @@ function App() {
     }
   )
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+
+    setTodos(newTodos)
+  }
+
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
       (todo) => todo.text === text
     );
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -43,7 +62,7 @@ function App() {
       (todo) => todo.text === text
     );   
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos)
+    saveTodos(newTodos)
   };
 
   console.log(searchValue)
